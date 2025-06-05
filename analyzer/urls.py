3cc43825642d0d4a7a7f7b22de900571116d5441
devRajeshThapa <devrajeshthapa@gmail.com>
2025-06-05 20:28:48 +0545

    add function

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import generate_analysis
+from .views import generate_analysis, upload_chunk
 
 urlpatterns = [
     path('generate-analysis/', generate_analysis, name='generate_analysis'),
+    path('upload_chunk/', upload_chunk, name='upload_chunk'),
 ]
