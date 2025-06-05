5a0d7db59004d533b9f74f4f6f947307cc0396ca
devRajeshThapa <devrajeshthapa@gmail.com>
2025-06-05 20:35:27 +0545

    debug

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -3,5 +3,5 @@ from .views import generate_analysis, upload_chunk
 
 urlpatterns = [
     path('generate-analysis/', generate_analysis, name='generate_analysis'),
-    path('upload_chunk/', upload_chunk, name='upload_chunk'),
+    path('upload-chunk/', upload_chunk, name='upload_chunk'),
 ]
